e22f1efc4b7dddd8c453feb7422e9d0b02919f7a
adrianpardodev <adpardo@hotmail.es>
2025-04-05 17:48:36 -0300

    GRUP05-25: secured logout route and removed sensitive logs

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -2,9 +2,10 @@ import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
 export async function GET() {
-  // Eliminar la cookie de sesión
-  cookies().set("session", "", { expires: new Date(0) });
+  const cookieStore = await cookies(); // <-- así sí
+  cookieStore.set("session", "", { expires: new Date(0) });
 
-  // Redirigir a la página de login
   return NextResponse.redirect("/login");
 }
+
+
