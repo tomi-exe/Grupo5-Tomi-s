107fe28e7261a51e4f86ebe0db17387ee36227df
tomi-exe <163452318+tomi-exe@users.noreply.github.com>
2025-06-25 16:45:25 -0400

    Add jest setup and unit test for ticket purchase

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+};
